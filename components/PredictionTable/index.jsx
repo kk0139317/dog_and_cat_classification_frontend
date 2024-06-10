8d8631f725e6c0cdb081745f803e24183fbdd0e1
kk0139317 <kk4881757@gmail.com>
2024-06-10 12:18:59 +0530

    modified:   components/PredictionTable/index.jsx 	modified:   components/UploadImage/index.jsx 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/live_dtetction/index.js

--- components/PredictionTable/index.jsx
+++ components/PredictionTable/index.jsx
@@ -26,28 +26,34 @@ const PredictionsTable = () => {
     };
 
     return (
-        <div className="container mx-auto mt-8">
-            <h2 className="text-3xl font-bold mb-6">Predictions Table</h2>
-            <div className="overflow-x-auto">
-                <table className="min-w-max w-full table-auto divide-y divide-gray-200 bg-white shadow-md rounded-lg overflow-hidden">
-                    <thead className="bg-blue-100 text-blue-800">
+        <div className="container mx-auto mt-8 px-4">
+            <h2 className="text-4xl font-bold text-center mb-6 text-gray-800">Predictions Table</h2>
+            <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+                <table className="min-w-full table-auto divide-y divide-gray-200">
+                    <thead className="bg-blue-500 text-white">
                         <tr>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Image</th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Prediction</th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Confidence</th>
                             <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Timestamp</th>
-                            <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">URL</th>
+                            <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Image Name</th>
+                            <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Original Dimensions</th>
+                            <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">Model Dimensions</th>
+                            <th scope="col" className="px-6 py-3 text-left text-xs font-semibold uppercase tracking-wider">View</th>
                         </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200">
                         {predictions.map(prediction => (
-                            <tr key={prediction.id} className="transition-colors duration-300 hover:bg-gray-100 cursor-pointer">
-                                <td className="px-6 py-4 whitespace-nowrap">
-                                    <img src={`http://127.0.0.1:8000/${prediction.image}`} alt="Prediction" className="h-16 w-16 object-cover rounded-full" onClick={() => openPopup(prediction)} />
+                            <tr key={prediction.id} className="transition duration-300 ease-in-out transform hover:bg-blue-100 cursor-pointer">
+                                <td className="px-2 pl-6 py-4 whitespace-nowrap">
+                                    <img src={`http://127.0.0.1:8000/${prediction.image}`} alt="Prediction" className="h-16 w-16 object-cover rounded-full border-2 border-blue-500" onClick={() => openPopup(prediction)} />
                                 </td>
                                 <td className="px-6 py-4 whitespace-nowrap">{prediction.prediction}</td>
                                 <td className="px-6 py-4 whitespace-nowrap">{prediction.confidence.toFixed(2)}%</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{prediction.timestamp}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{new Date(prediction.timestamp).toLocaleString()}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{prediction.image_name}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{prediction.original_height} X {prediction.original_width}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{prediction.model_height} X {prediction.model_width}</td>
                                 <td className="px-6 py-4 whitespace-nowrap"><a href="#" className="text-blue-600 hover:underline" onClick={() => openPopup(prediction)}>View</a></td>
                             </tr>
                         ))}
@@ -57,22 +63,25 @@ const PredictionsTable = () => {
             {/* Popup Card */}
             {selectedPrediction && (
                 <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-                    <div className="bg-white rounded-lg p-8 max-w-lg w-full shadow-md">
+                    <div className="relative bg-white rounded-lg p-8 max-w-3xl w-full shadow-lg">
                         <button className="absolute top-4 right-4 text-gray-500 hover:text-gray-700" onClick={closePopup}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
-                        <h3 className="text-xl font-bold mb-4">Prediction Details</h3>
-                        <div className="grid grid-cols-2 gap-4">
-                            <div className="col-span-2 sm:col-span-1">
-                                <img src={`http://127.0.0.1:8000/${selectedPrediction.image}`} alt="Prediction" className="h-64 w-full object-cover rounded-lg" />
+                        <h3 className="text-2xl font-bold mb-4 text-gray-800">Prediction Details</h3>
+                        <div className="flex flex-col sm:flex-row">
+                            <div className="flex-shrink-0">
+                                <img src={`http://127.0.0.1:8000/${selectedPrediction.image}`} alt="Prediction" className="h-64 w-full sm:w-64 object-cover rounded-lg border-2 border-gray-300" />
                             </div>
-                            <div className="col-span-2 sm:col-span-1">
-                                <p className="font-semibold">Prediction: <span className="text-gray-700">{selectedPrediction.prediction}</span></p>
-                                <p className="font-semibold">Confidence: <span className="text-gray-700">{selectedPrediction.confidence.toFixed(2)}%</span></p>
-                                <p className="font-semibold">Timestamp: <span className="text-gray-700">{selectedPrediction.timestamp}</span></p>
-                                <p className="font-semibold">URL: <a href={selectedPrediction.url} className="text-blue-600 hover:underline">{selectedPrediction.url}</a></p>
+                            <div className="sm:ml-6 mt-4 sm:mt-0 space-y-2">
+                                <p className="font-semibold text-lg">Prediction: <span className="text-gray-700">{selectedPrediction.prediction}</span></p>
+                                <p className="font-semibold text-lg">Confidence: <span className="text-gray-700">{selectedPrediction.confidence.toFixed(2)}%</span></p>
+                                <p className="font-semibold text-lg">Timestamp: <span className="text-gray-700">{new Date(selectedPrediction.timestamp).toLocaleString()}</span></p>
+                                <p className="font-semibold text-lg">Image Name: <span className="text-gray-700">{selectedPrediction.image_name}</span></p>
+                                <p className="font-semibold text-lg">Original Dimensions: <span className="text-gray-700">{selectedPrediction.original_height} x {selectedPrediction.original_width}</span></p>
+                                <p className="font-semibold text-lg">Model Dimensions: <span className="text-gray-700">224 x 224</span></p>
+                                <p className="font-semibold text-lg">URL: <a href={selectedPrediction.url} className="text-blue-600 hover:underline break-all">{selectedPrediction.url}</a></p>
                             </div>
                         </div>
                     </div>
--- components/UploadImage/index.jsx
+++ components/UploadImage/index.jsx
@@ -6,28 +6,33 @@ const UploadImage = () => {
     const [showPopup, setShowPopup] = useState(false);
     const [prediction, setPrediction] = useState(null);
     const [error, setError] = useState(null);
+    const [file, setFile] = useState(null);
+    const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 });
+    const [imageName, setImageName] = useState('');
 
-    // const handleImageUpload = async (e) => {
-    //     const file = e.target.files[0];
-    //     const formData = new FormData();
-    //     formData.append('file', file);
+    const MODEL_WIDTH = 224;
+    const MODEL_HEIGHT = 224;
 
-    //     try {
-    //         const response = await axios.post('http://localhost:8000/api/upload/', formData, {
-    //             headers: {
-    //                 'Content-Type': 'multipart/form-data',
-    //             },
-    //         });
-    //         setPrediction(response.data);
-    //         setImage(URL.createObjectURL(file));
-    //         setShowPopup(true);
-    //     } catch (error) {
-    //         console.error('Error uploading image:', error);
-    //         setError('Error uploading image. Please try again.');
-    //     }
-    // };
-    const handleImageUpload = async (e) => {
-        const file = e.target.files[0];
+    const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+        const imageUrl = URL.createObjectURL(selectedFile);
+        setImage(imageUrl);
+        setImageName(selectedFile.name);
+
+        const img = new Image();
+        img.onload = () => {
+            setImageDimensions({ width: img.width, height: img.height });
+        };
+        img.src = imageUrl;
+    };
+
+    const handleSubmit = async () => {
+        if (!file) {
+            setError('Please select an image.');
+            return;
+        }
+
         const formData = new FormData();
         formData.append('file', file);
         formData.append('url', window.location.href); // Send the URL of the page
@@ -39,13 +44,14 @@ const UploadImage = () => {
                 },
             });
             setPrediction(response.data);
-            setImage(URL.createObjectURL(file));
             setShowPopup(true);
+            setError(null);
         } catch (error) {
             console.error('Error uploading image:', error);
             setError('Error uploading image. Please try again.');
         }
     };
+
     return (
         <div className="flex items-center justify-center min-h-screen bg-white">
             <div className="bg-gray-100 rounded-lg shadow-lg p-8 w-full max-w-md">
@@ -54,7 +60,7 @@ const UploadImage = () => {
                     <input
                         type="file"
                         accept="image/*"
-                        onChange={handleImageUpload}
+                        onChange={handleFileChange}
                         className="hidden"
                         id="upload-input"
                     />
@@ -62,10 +68,30 @@ const UploadImage = () => {
                         Upload Image
                     </label>
                 </div>
+                {image && (
+                    <div className="mb-6 text-center">
+                        <div className="border border-gray-300 p-4 rounded-lg inline-block">
+                            <img src={image} alt="Uploaded" className="rounded-lg mb-4" style={{ maxWidth: '100%' }} />
+                            <p className="text-gray-700">Name: {imageName}</p>
+                            <p className="text-gray-700">Original Dimensions: {imageDimensions.width}px (W) x {imageDimensions.height}px (H)</p>
+                            <p className="text-gray-700">Model Dimensions: {MODEL_WIDTH}px (W) x {MODEL_HEIGHT}px (H)</p>
+                        </div>
+                    </div>
+                )}
                 {error && <p className="text-red-600 mb-4 text-center">{error}</p>}
+                {image && (
+                    <div className="text-center">
+                        <button
+                            onClick={handleSubmit}
+                            className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-full transition duration-300"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                )}
                 {showPopup && (
                     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                        <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
+                        <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md relative">
                             <button
                                 className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
                                 onClick={() => setShowPopup(false)}
@@ -74,6 +100,9 @@ const UploadImage = () => {
                             </button>
                             <div className="flex flex-col items-center">
                                 <img src={image} alt="Uploaded" className="rounded-lg mb-6 border border-gray-300" style={{ maxWidth: '300px' }} />
+                                <p className="text-gray-700">Name: {imageName}</p>
+                                <p className="text-gray-700">Original Dimensions: {imageDimensions.width}px (W) x {imageDimensions.height}px (H)</p>
+                                <p className="text-gray-700">Model Dimensions: {MODEL_WIDTH}px (W) x {MODEL_HEIGHT}px (H)</p>
                                 {prediction ? (
                                     <div className="text-center">
                                         <h2 className="text-2xl font-bold mb-2 text-gray-800">Prediction Result</h2>
